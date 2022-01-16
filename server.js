387ed64da036edc4ada3ff30e20d860a4d3b083d
muzamil <mdmuzamilldcme887@gmail.com>
2022-01-16 15:17:59 +0530

    commit

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/app/'}),
 );
 
-// Start the app by listening on the default Heroku port
+// Start the app by listening on the default Heroku PORT
 app.listen(process.env.PORT || 8080);
