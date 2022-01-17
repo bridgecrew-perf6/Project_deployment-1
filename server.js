7bddc6977b57a149d3acfe2acd9b97a5b582120d
muzamil <mdmuzamilldcme887@gmail.com>
2022-01-17 13:51:32 +0530

    comit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/appointments'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app/'}),
+    res.sendFile('index.html', {root: 'dist/appointments/'}),
 );
 
 // Start the app by listening on the default Heroku PORT
