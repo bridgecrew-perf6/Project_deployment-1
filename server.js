2311ff4f441e2dc39c22b34b260d44e8932eb976
muzamil <mdmuzamilldcme887@gmail.com>
2022-01-17 13:56:31 +0530

    commit

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/appointments'));
+app.use(express.static(__dirname,"./dist/appointments"));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/appointments/'}),
 );
 
 // Start the app by listening on the default Heroku PORT
-let port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`this application is running on port no ${port}`));
+//let port = process.env.PORT || 8080;
+//app.listen(port, () => console.log(`this application is running on port no ${port}`));
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
